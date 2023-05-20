155e6ebc3605ab256fe08af6da2480e19c00ed08
Vladyslav Kupchuk <kupchuk.vlad@gmail.com>
2023-05-20 20:47:54 +0300

    CHANGED: README.md; ADDED: commander in index.js

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # goit-node-hw
+
+## 1. Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+
+```shell
+node index.js --action="list"
+```
+
+<a href="https://prnt.sc/gB16wGEcv9Ml">Photo</a>
+
+## 2. Отримуємо контакт по id
+
+```shell
+node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+```
+
+<a href="https://prnt.sc/0xWYf3PQFvZ8">Photo</a>
+
+## 3. Додаємо контакт
+
+```shell
+node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+```
+
+<a href="https://prnt.sc/wULjwWPH1yQ8">Photo</a>
+
+## 4. Видаляємо контакт
+
+```shell
+node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH
+```
+
+<a href="https://prnt.sc/7uI1PZsCTBCj">Photo</a>
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const {listContacts, getContactById, addContact, removeContact} = require('./contacts')
+const { Command } = require("commander");
+const program = new Command();
+
+const {
+	listContacts,
+	getContactById,
+	addContact,
+	removeContact
+} = require('./contacts')
 
 
 async function invokeAction({ action, id, name, email, phone }) {
 	switch (action) {
 		case "list":
 			const contacts = await listContacts();
-			console.log(contacts)
+			console.table(contacts)
 		break;
 
 		case "get":
@@ -28,12 +36,14 @@ async function invokeAction({ action, id, name, email, phone }) {
 	}
 }
 
-const action = {
-	action: 'list',
-	// id: 'qMvp4_FFu5s766ANhKU5K',
-    // name: "Alec Howard",
-    // email: "Donec.elementum@scelerisquescelerisquedui.net",
-    // phone: "(748) 206-2688"
-}
+program
+	.option("-a, --action <type>", "choose action")
+	.option("-i, --id <type>", "user id")
+	.option("-n, --name <type>", "user name")
+	.option("-e, --email <type>", "user email")
+	.option("-p, --phone <type>", "user phone");
 
-invokeAction(action);
+program.parse(process.argv);
+
+const argv = program.opts();
+invokeAction(argv);
